7f7c170b5898eff29f334c7d9e14f2e151fc2bf5
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2020-06-24 01:04:20 +0530

    Started Knight_Rider_effect coding

---
+++ Knight_Rider_effect/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,17 @@
+/*
+ * Created: 6/24/2020 1:01:06 AM
+ * Author : Bimalka Piyaruwan Thalagala
+ * This project is an extension of Blink_an_LED project, where 8 LEDs are used to generate a pattern.
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+   
+    while (1) 
+    {
+    }
+}
+
