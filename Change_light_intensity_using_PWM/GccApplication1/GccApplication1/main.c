fb01bda5d1760e5bbf3c3c1b800f7449e28328a2
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2020-06-25 21:59:00 +0530

    Initiated two projects

---
+++ Change_light_intensity_using_PWM/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,70 @@
+/*
+ * Change_light_intensity_using_PWM
+ * Created: 6/25/2020 3:03:22 PM
+ * Author : Bimalka Piyaruwan Thalagala
+ */
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+
+    while (1)
+    {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#define F_CPU 8000000UL
+#include "avr/io.h"
+#include <util/delay.h>
+
+void PWM_init()
+{
+	/*set fast PWM mode with non-inverted output*/
+	TCCR0 = (1<<WGM00) | (1<<WGM01) | (1<<COM01) | (1<<CS00);
+	DDRB|=(1<<PB3);	/*set OC0 pin as output*/
+}
+
+
+int main ()
+{
+	unsigned char duty;
+
+	PWM_init();
+	while (1)
+	{
+		for(duty=0; duty<255; duty++)
+		{
+			OCR0=duty;			/*increase the LED light intensity*/
+			_delay_ms(8);
+		}
+		for(duty=255; duty>1; duty--)
+		{
+			OCR0=duty;			/*decrease the LED light intensity*/
+			_delay_ms(8);
+		}
+	}
+}
+
+*/
---
+++ Time_delaying_without_using_BuiltIn_Functions/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,17 @@
+/*
+ * Time delaying without using built-in functions 
+ *
+ * Created: 6/25/2020 9:55:54 PM
+ * Author : Bimalka Piyaruwan Thalagala
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{ 
+    while (1) 
+    {
+    }
+}
+
